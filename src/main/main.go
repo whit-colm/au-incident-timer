2a4b2b6ae86bd68562f94eeeb18f9b5e687d188c
Whitman Huntley <whitman.colm@gmail.com>
2018-10-09 09:22:31 -0400

    save

--- src/main/main.go
+++ src/main/main.go
@@ -1,31 +1,107 @@
 package main
 
 import (
+	"github.com/aurumbot/flags"
 	"github.com/aurumbot/lib/dat"
 	f "github.com/aurumbot/lib/foundation"
 	dsg "github.com/bwmarrin/discordgo"
 )
 
 var config struct {
-	myField string `json:"myfield"`
+	Guild map[string]struct {
+		Topic []struct {
+			Name     string        `json:"name"`
+			Triggers []string      `json:"triggers"`
+			Since    time.Time     `json:"since"`
+			User     string        `json:"user"`
+			Message  string        `json:"message"`
+			Streak   time.Duration `json:"streak"`
+		} `json:"topic"`
+	} `json:"guild"`
 }
 
 var Commands = make(map[string]*f.Command)
 
 func init() {
-	Commands["commandname"] = &f.Command{
-		Name:    "Somename",
-		Help:    "Help page, make sure to define flags and other noteworthy things",
-		Perms:   -1,
+	Commands["bpconfig"] = &f.Command{
+		Name: "The Bad Post Configuration Tool",
+		Help: `bpconfig is the modificaation tool for the bad post timer. It is used to define
+topics and triggers (as regular expressions). If you have retained vestages of
+your sanity but want to learn regex, try https://regexr.com.
+# Arguments
+**-new <topic name>** : Create a new topic with the topic name <topic name>
+**-rm <topic name>**  : Remove the <topic name>. Dangerous!
+**-ls** : List all topics
+**<topic name> <--addtrigger|-a> <//regex//>** : Add a trigger to <topic name> fulfilling //regex// (must be in //double slashes//)
+**<topic name> <--rmtrigger|-rm> <item number>** : remove the <item number> trigger from <topic name>
+**<topic name> <-ls>** : List all topic triggers
+# Usage:
+` + f.Config.Prefix + `bpconfig -new foo
+` + f.Config.Prefix + `bpconfig foo -a //^[A-z].+$//`,
+		Perms:   dsg.PermissionManageWebhooks,
 		Version: "v1.0",
-		Action:  command,
+		Action:  bpconfig,
 	}
 
-	dat.Load("exampleplugin/prefs.json" & config)
+	dat.Load("incident-counter/config.json", &config)
+	if config.Guild == nil {
+		config.Guild = make(map[string]struct {
+			Topic []struct {
+				Name     string        `json:"name"`
+				Triggers []string      `json:"triggers"`
+				Since    time.Time     `json:"since"`
+				User     string        `json:"user"`
+				Message  string        `json:"message"`
+				Streak   time.Duration `json:"streak"`
+			} `json:"topic"`
+		})
+	}
+
+	f.Session.AddHandler(incidentHandler)
 }
 
-// Must have these arguments with these names.
-func command(session *dsg.Session, message *dsg.Message) {
-	session.ChannelMessageSend(message.ChannelID, "Hello, World!")
-	dat.Log.Println("Pass errors here to be logged.")
+func incidentHandler(s *dsg.Session, m *dsg.MessageCreate) {
+	if message.Author.Bot {
+		return
+	}
+	var guildID string
+	if channel, err := session.Channel(m.Message.ChannelID); err != nil {
+		dat.Log.Println(err)
+		return
+	} else {
+		guildID = channel.GuildID
+	}
+	// Local topics reduces verbosity by grabbing the config.Guild[guildID] value.
+	localTopics := config.Guild[guildID].Topics
+	for li := range localTopics {
+		for i := range localTopics[li].Triggers {
+			match, err := regexp.Compile(localTopics[li].Triggers[i])
+			if err != nil {
+				dat.Log.Println(err)
+				return
+			}
+
+		}
+	}
+}
+
+func bpconfig(session *dsg.Session, message *dsg.Message) {
+	var guildID string
+	if channel, err := session.Channel(message.ChannelID); err != nil {
+		dat.Log.Println(err)
+		return
+	} else {
+		guildID = channel.GuildID
+	}
+	// Local topics reduces verbosity by grabbing the config.Guild[guildID] value.
+	localTopics := config.Guild[guildID].Topics
+
+	flgs := flags.Parse(message.Content)
+	for i := range flgs {
+
+	}
+}
+
+func removeTopicConfirm(s *dsg.Session, m *dsg.MessageCreate) {
+
 }
